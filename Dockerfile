b2d243ed1e7b8d98510f17aa592af57243d088da
Artem Kovalchuk <kovart.dev@gmail.com>
2022-06-25 22:22:22 +0300

    add linters and common files

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,10 @@ RUN npm run build
 FROM node:12-alpine
 ENV NODE_ENV=production
 # Uncomment the following line to enable agent logging
-# LABEL "network.forta.settings.agent-logs.enable"="true"
+LABEL "network.forta.settings.agent-logs.enable"="true"
 WORKDIR /app
 COPY --from=builder /app/dist ./src
+COPY bot-config.json ./
 COPY package*.json ./
 RUN npm ci --production
 CMD [ "npm", "run", "start:prod" ]
--- src/agent.ts
+++ src/agent.ts
@@ -1,17 +1,57 @@
+import { providers } from 'ethers';
 import {
   Finding,
+  Initialize,
   HandleTransaction,
   TransactionEvent,
-} from "forta-agent";
+  getEthersProvider,
+} from 'forta-agent';
+import { Logger, LoggerLevel } from './logger';
+import { DataContainer } from './types';
+import { createFinding } from './findings';
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
+const data: DataContainer = {} as any;
+const provider = getEthersProvider();
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const logger = new Logger(isDevelopment ? LoggerLevel.DEBUG : LoggerLevel.WARN);
+const botConfig = require('../bot-config.json');
 
-  return findings;
+const provideInitialize = (
+  data: DataContainer,
+  provider: providers.JsonRpcProvider,
+  logger: Logger,
+  isDevelopment: boolean,
+): Initialize => {
+  return async function initialize() {
+    data.logger = logger;
+    data.provider = provider;
+    data.isDevelopment = isDevelopment;
+    data.isInitialized = true;
+
+    logger.debug('Initialized');
+  };
+};
+
+const provideHandleTransaction = (data: DataContainer): HandleTransaction => {
+  return async function handleTransaction(txEvent: TransactionEvent) {
+    if (!data.isInitialized) throw new Error('DataContainer is not initialized');
+
+    const findings: Finding[] = [];
+
+    data.logger.debug('Transaction', txEvent.hash);
+
+    if (Math.random() <= 0.1) {
+      findings.push(createFinding());
+    }
+
+    return findings;
+  };
 };
 
 export default {
-  handleTransaction
+  initialize: provideInitialize(data, provider, logger, isDevelopment),
+  handleTransaction: provideHandleTransaction(data),
+
+  provideInitialize,
+  provideHandleTransaction,
 };
